7b279aedcda2cffd73d949b13b2231143038cb04
Ko0lGuY <bha0011@jmss.vic.edu.au>
2017-06-30 20:19:15 +1000

    deleted unnecessary files

--- bkp mdl/scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('a').click(function(){
-    $(".mdl-layout__content").animate({
-        scrollTop: $( $(this).attr('href') ).offset().top-30
-    }, 500);
-    return false;
-});
-});
--- bkp mdl/scrolled.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-$(function(){
-    $('#inner-content-div').slimScroll({
-        height: '250px'
-    });
-});
-});
